6100cac48a5ce1e7ac4e760847a27b76897295d4
connorphu <connorphu@gmail.com>
2018-03-03 11:08:07 -0500

    simple table

---
+++ ThatOneStore/ThatOneStore.Context/ThatOneStoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThatOneStore.Library.Models;
+
+namespace ThatOneStore.Context
+{
+    public class ThatOneStoreContext : DbContext
+    {
+        public ThatOneStoreContext(DbContextOptions<ThatOneStoreContext> options) : base(options) { }
+
+        public DbSet<Person> Peeps { get; set; }
+    }
+}
---
+++ ThatOneStore/ThatOneStore.Library/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ThatOneStore.Library.Models
+{
+    [Table("Person", Schema = "caikaro")]
+    public class Person
+    {
+        [Key]
+        [Column("PersonId")]
+        int PersonId { get; set; }
+
+        [Column("Name")]
+        string Name { get; set; }
+    }
+}
